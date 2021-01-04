ed93951386c09eafda7aad6c3dc477c1cfc35672
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-01-04 23:01:35 +0100

    1st, 2nd

---
+++ Encrypt_Sort_and_Print_Array/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Linq;
+
+namespace Encrypt_Sort_and_Print_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] names = new int [n];
+            int sum = 0;
+            int current = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                for (int k = 0; k < name.Length; k++)
+                {
+                    switch (name[k])
+                    {
+                        case 'u':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'U':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'a':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'A':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'i':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'I':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'o':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'O':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'e':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+                        case 'E':
+                            current = name[k] * name.Length;
+                            sum += current;
+                            break;
+
+                        default:
+                            current = name[k] / name.Length;
+                            sum += current;
+                            break;
+                    }
+                }
+                names[i] = sum;
+                sum = 0;
+            }
+            int temp;
+
+            for (int i = 0; i < names.Length - 1; i++)
+            {
+                for (int j = i + 1; j < names.Length; j++)
+                {
+                    if (names[i] > names[j])
+                    {
+
+                        temp = names[i];
+                        names[i] = names[j];
+                        names[j] = temp;
+
+                    }
+
+                }
+
+            }
+            foreach (var item in names)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
---
+++ Pascal_Triangle/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pascal_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dimension = int.Parse(Console.ReadLine());
+            Console.WriteLine(1);
+            int[] previous = new int[1];
+            for (int i = 2; i <= dimension; i++)
+            {
+                int[] values = new int[i];
+                values[0] = 1;
+                values[i - 1] = 1;
+                for (int j = 1; j < i - 1; j++)
+                {
+                    values[j] = previous[j - 1] + previous[j];
+                }
+                Console.WriteLine(string.Join(' ', values));
+                previous = values;
+            }
+        }
+    }
+}
