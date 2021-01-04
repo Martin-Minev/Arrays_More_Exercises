57a0abb2b36fc64f472e2784f6c614943ab65278
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-01-05 00:40:41 +0100

    4th

---
+++ Fold_and_Sum/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace Fold_and_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                 .Split()
+                 .Select(int.Parse)
+                 .ToArray();
+            int[] results = new int[numbers.Length / 2];
+
+            for (int i = 0; i < numbers.Length / 4; i++)
+            {
+                results[i] = numbers[(numbers.Length / 4) + i] + numbers[(numbers.Length / 4) - 1 - i];
+            }
+
+            for (int i = (numbers.Length / 2) - 1; i >= numbers.Length / 4; i--)
+            {
+                results[i] = numbers[(numbers.Length / 4) + i] + numbers[(numbers.Length + numbers.Length / 4) - 1 - i];
+            }
+
+            Console.WriteLine(string.Join(" ", results));
+        }
+    }
+}
