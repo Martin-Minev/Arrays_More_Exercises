ea4d61d00f8e804661d0a2b8133e1980d31169a9
Martin-Minev <72812000+Martin-Minev@users.noreply.github.com>
2021-01-04 23:21:56 +0100

    3rd

---
+++ Recursive_Fibonacci/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Recursive_Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] digits = new int[n];
+            digits[0] = 1;
+            digits[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                digits[i] = digits[i - 1] + digits[i-2];
+            }
+            Console.WriteLine(digits[n-1]);
+        }
+    }
+}
